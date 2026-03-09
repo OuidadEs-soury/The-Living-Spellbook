b9218a5fc17db433d2040fc096c834f18620d0a2
Ouidad7 <widadelsouri@gmail.com>
2026-03-09 22:34:22 +0000

    spell system

--- script.js
+++ script.js
@@ -9,3 +9,38 @@ darkness.style.background =
 `radial-gradient(circle 150px at ${x}px ${y}px, transparent, black 80%)`
 
 })
+const spells = document.querySelectorAll(".spell")
+
+spells.forEach(button => {
+
+button.addEventListener("click", ()=>{
+
+let spell = button.dataset.spell
+
+castSpell(spell)
+
+})
+
+})
+
+function castSpell(spell){
+
+if(spell === "lumos"){
+
+document.body.style.background = "#111"
+
+}
+
+if(spell === "accio"){
+
+alert("Objects fly toward your wand!")
+
+}
+
+if(spell === "alohomora"){
+
+alert("🔓 The magic unlocks hidden doors")
+
+}
+
+}
